aebed462735f79229575da4e147274dad051aca7
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-05-20 14:09:59 +0200

    feat: continuing tiny malloc

--- src/malloc.c
+++ src/malloc.c
@@ -1,28 +1,39 @@
 #include "malloc.h"
 
-#include <sys/mman.h> // MAP_ANONYMOUS, MAP_PRIVATE, PROT_READ, PROT_WRITE, mmap()
+#include <sys/mman.h> // MAP_ANONYMOUS, MAP_FAILED, MAP_PRIVATE, PROT_READ, PROT_WRITE, mmap()
 
 #include <stddef.h> // size_t, NULL
 #include <stdio.h> // perror()
 #include <unistd.h> // getpagesize()
 
-void *malloc(size_t size)
+s_zone *create_tiny_zone(size_t size)
 {
-	if (size <= TINY_MAX)
-		return allocate_tiny(size);
-	else if (size <= SMALL_MAX)
-		return allocate_small(size);
-	return allocate_large(size);
+	s_zone *zone;
+
+	zone = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	if (zone == MAP_FAILED)
+		return (perror("mmap()"), NULL);
+
+	zone->chunk = 
+
+	return (zone);
 }
 
 void *allocate_tiny(size_t size)
 {
-	t_zone *zone;
-	t_chunk *chunk;
+	s_zone *zone;
+	s_chunk *chunk;
 
-	zone = find_tiny_zone(size);
-	if (!zone)
-		zone = create_tiny_zone();
+	zone = create_tiny_zone();
 
 	chunk = find_free_chunk()
+}
+
+void *malloc(size_t size)
+{
+	if (size <= TINY_MAX)
+		return allocate_tiny(size);
+	//if (size <= SMALL_MAX)
+	//	return allocate_small(size);
+	//return allocate_large(size);
 }
