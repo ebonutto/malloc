57709f33bfe23393334235298cfe48b7f89df3a8
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-05-21 17:45:55 +0200

    fix: cleaning .h

--- include/malloc.h
+++ include/malloc.h
@@ -8,26 +8,35 @@
 #define TINY_MAX 128
 #define SMALL_MAX 1024
 
+#define TINY_ZONE_SIZE TINY_MAX * 100
+
 /* Structures */
 typedef struct s_chunk {
 	size_t size;
-	struct s_chunk *next;
 	struct s_chunk *prev;
+	struct s_chunk *next;
 	unsigned char free;
 } t_chunk;
 
 typedef struct s_zone {
 	size_t total_size;
-	struct s_chunk *chunks;
+	t_chunk *chunks;
 	struct s_zone *next;
 } t_zone;
 
 /* Global variables */
-static t_zone *g_zone_tiny = NULL;
-static t_zone *g_zone_small = NULL;
-static t_zone *g_zone_large = NULL;
+extern t_zone *g_zone_tiny;
+extern t_zone *g_zone_small;
+extern t_zone *g_zone_large;
 
 /* Prototypes */
+/* malloc.c */
 void *malloc(size_t size);
+
+/* zone.c */
+t_zone *create_zone(size_t size);
+
+/* print.c */
+void show_alloc_mem_ex(void);
 
 #endif
