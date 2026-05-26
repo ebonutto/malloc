bc0797caece710ffe740fdba8e6e20dd97b1e3c3
Elio Bonutto <elio@mbp-de-elio.home>
2026-05-26 23:47:40 +0200

    fix: removing show_log for now

--- src/show_ex.c
+++ src/show_ex.c
@@ -3,7 +3,6 @@
 #include <stddef.h> // size_t
 #include <stdio.h> // printf()
 
-static void show_alloc_history(void);
 static void show_data(void);
 static size_t show_zones(t_zone *g_zone, const char *zone_name);
 
@@ -12,7 +11,6 @@ void show_alloc_mem_ex(void)
 	size_t total;
 
 	show_data();
-	show_alloc_history();
 	total = 0;
 	total += show_zones(g_malloc.tiny, "TINY");
 	total += show_zones(g_malloc.small, "SMALL");
@@ -25,14 +23,6 @@ static void show_data(void)
 	printf("--- DATA ---\n");
 	printf("  sizeof(t_zone) : %zu bytes\n", sizeof(t_zone));
 	printf("  sizeof(t_chunk) : %zu bytes\n", sizeof(t_chunk));
-}
-
-static void show_alloc_history(void)
-{
-	// t_log logs;
-	printf("--- ALLOC HISTORY ---\n");
-	
-
 }
 
 static void hexdump(const void *ptr, size_t size)
