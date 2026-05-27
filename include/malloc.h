a50b99aac2d5c7347a9a3f19c3e046da80e6705c
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-05-27 11:19:33 +0200

    feat: add protos

--- include/malloc.h
+++ include/malloc.h
@@ -5,9 +5,11 @@
 #include <stddef.h> // size_t
 
 /* Prototypes */
+void *calloc(size_t count, size_t size);
 void *malloc(size_t size);
-void free(void *ptr);
 void *realloc(void *ptr, size_t size);
+
+void free(void *ptr);
 
 void show_alloc_mem(void);
 void show_alloc_mem_ex(void);
