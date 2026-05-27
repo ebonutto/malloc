1aac676903d7eaf591c20740ba5fb5ca2c8be987
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-05-27 15:13:07 +0200

    feat: add tests

--- src/main.c
+++ src/main.c
@@ -2,24 +2,67 @@
 
 #include <stdio.h> // printf()
 
+void test_tiny(void);
+void test_small(void);
+
 int main()
 {
-	const char *hello = "Hello World!";
-	char *str;
-	int i = 0;
-
-	str = malloc(1025);
-	i = 0;
-	while (hello[i]) {
-		str[i] = hello[i];
-		i++;
-	}
-	str[i] = '\0';
-
-
-	show_alloc_mem_ex();
-
-	free(str);
-	show_alloc_mem_ex();
+	test_small();
 	return (0);
+}
+
+void test_tiny(void)
+{
+	void *p1;
+	void *p2;
+	void *p3;
+	void *p4;
+
+	p1 = malloc(1);
+	p2 = malloc(16);
+	p3 = malloc(42);
+	p4 = malloc(128);
+
+	show_alloc_mem_ex();
+
+	free(p1);
+	free(p3);
+
+	show_alloc_mem_ex();
+
+	free(p4);
+
+	show_alloc_mem_ex();
+
+	free(p2);
+
+	show_alloc_mem_ex();
+}
+
+void test_small(void)
+{
+	void *p1;
+	void *p2;
+	void *p3;
+	void *p4;
+
+	p1 = malloc(129);
+	p2 = malloc(200);
+	p3 = malloc(512);
+	p4 = malloc(1024);
+
+	show_alloc_mem_ex();
+
+	free(p1);
+	free(p3);
+
+	show_alloc_mem_ex();
+
+	free(p4);
+
+	show_alloc_mem_ex();
+
+	free(p2);
+
+	show_alloc_mem_ex();
 }
