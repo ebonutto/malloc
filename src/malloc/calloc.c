1fae1fa2a871cc6e389059b94b5887cc2769fcc1
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-05-27 11:06:11 +0200

    new: calloc()

---
+++ src/malloc/calloc.c
@@ -0,0 +1,18 @@
+#include "malloc_int.h"
+
+#include <stdint.h> // SIZE_MAX
+#include <strings.h> // bzero()
+
+void *calloc(size_t count, size_t size)
+{
+	void *ptr;
+
+	if (count != 0 && size > SIZE_MAX / count)
+		return (NULL);
+	size = count * size;
+	ptr = malloc(size);
+	if (!ptr)
+		return (NULL);
+	bzero(ptr, size);
+	return (ptr);
+}
