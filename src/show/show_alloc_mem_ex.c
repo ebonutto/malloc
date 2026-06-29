3259a9c95d1e6478a509717c78a4aeedce74493a
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-06-29 17:20:41 +0200

    feat: show functions done

--- src/show/show_alloc_mem_ex.c
+++ src/show/show_alloc_mem_ex.c
@@ -4,86 +4,86 @@
 #include <stddef.h> // size_t
 #include <stdio.h> // printf()
 
-//static void show_data(void)
-//{
-//	printf("--- DATA ---\n");
-//	printf("  sizeof(t_zone) : %zu bytes\n", sizeof(t_zone));
-//	printf("  sizeof(t_chunk) : %zu bytes\n", sizeof(t_chunk));
-//}
+static void show_data(void)
+{
+	printf("--- DATA ---\n");
+	printf("  sizeof(t_zone) : %zu bytes\n", sizeof(t_zone));
+	printf("  sizeof(t_chunk) : %zu bytes\n", sizeof(t_chunk));
+}
 
-//static void hexdump(const void *ptr, size_t size)
-//{
-//	const unsigned char *p;
-//	size_t i;
-//	size_t j;
+static void hexdump(const void *ptr, size_t size)
+{
+	const unsigned char *p;
+	size_t i;
+	size_t j;
 
-//	p = ptr;
-//	i = 0;
-//	for (i = 0; i < size; i += 16) {
-//		printf("      %04zx  ", i);
-//		for (j = 0; j < 16; j++) {
-//			if (i + j < size)
-//				printf("%02x ", p[i + j]);
-//			else
-//				printf("   ");
-//			if (j == 7)
-//				printf(" ");
-//		}
-//		printf(" ");
-//		for (j = 0; j < 16 && i + j < size ; j++)
-//			printf("%c",
-//			       (p[i+j] >= 32 && p[i+j] < 127) ? p[i+j] : '.');
-//		printf("\n");
-//	}
-//}
+	p = ptr;
+	i = 0;
+	for (i = 0; i < size; i += 16) {
+		printf("      %04zx  ", i);
+		for (j = 0; j < 16; j++) {
+			if (i + j < size)
+				printf("%02x ", p[i + j]);
+			else
+				printf("   ");
+			if (j == 7)
+				printf(" ");
+		}
+		printf(" ");
+		for (j = 0; j < 16 && i + j < size ; j++)
+			printf("%c",
+			       (p[i+j] >= 32 && p[i+j] < 127) ? p[i+j] : '.');
+		printf("\n");
+	}
+}
 
-//static size_t show_chunks(t_chunk *chunk)
-//{
-//	size_t total;
-//	void *start;
-//	void *end;
+static size_t show_chunks(t_chunk *chunk)
+{
+	size_t total;
+	void *start;
+	void *end;
 
-//	total = 0;
-//	while (chunk) {
-//		start = (void *)(chunk + 1);
-//		end = (void *)((char *)start + chunk->size);
-//		printf("    --- CHUNK : %p - %p : %zu bytes (free : %zu)\n",
-//		       start, end, chunk->size, chunk->flags & CHUNK_FREE);
-//		if (!(chunk->flags & CHUNK_FREE))
-//			hexdump(start, chunk->size);
-//		total += chunk->size;
-//		chunk = chunk->next;
-//	}
-//	return (total);
-//}
+	total = 0;
+	while (chunk) {
+		start = (void *)(chunk + 1);
+		end = (void *)((char *)start + chunk->size);
+		printf("    --- CHUNK : %p - %p : %zu bytes (free : %zu)\n",
+		       start, end, chunk->size, chunk->flags & CHUNK_FREE);
+		if (!(chunk->flags & CHUNK_FREE))
+			hexdump(start, chunk->size);
+		total += chunk->size;
+		chunk = chunk->next;
+	}
+	return (total);
+}
 
-//static size_t show_zones(t_zone *g_zone, const char *zone_name)
-//{
-//	t_zone *zone;
-//	size_t total;
+static size_t show_zones(t_zone *g_zone, const char *zone_name)
+{
+	t_zone *zone;
+	size_t total;
 
-//	printf("--- %s : %p ---\n", zone_name, (void *)g_zone);
-//	zone = g_zone;
-//	total = 0;
-//	while (zone) {
-//		printf("  --- ZONE : %p : %zu bytes ---\n",
-//		       (void *)zone, zone->size);
-//		total += show_chunks(zone->chunks);
-//		zone = zone->next;
-//	}
-//	return (total);
-//}
+	printf("--- %s : %p ---\n", zone_name, (void *)g_zone);
+	zone = g_zone;
+	total = 0;
+	while (zone) {
+		printf("  --- ZONE : %p : %zu bytes ---\n",
+		       (void *)zone, zone->size);
+		total += show_chunks(zone->chunks);
+		zone = zone->next;
+	}
+	return (total);
+}
 
 static void show_alloc_mem_ex_impl(void)
 {
 	size_t total;
 
-	//show_data();
+	show_data();
 	total = 0;
-	//total += show_zones(g_malloc.tiny, "TINY");
-	//total += show_zones(g_malloc.small, "SMALL");
-	//total += show_zones(g_malloc.large, "LARGE");
-	if (g_malloc.history.count)
+	total += show_zones(g_malloc.tiny, "TINY");
+	total += show_zones(g_malloc.small, "SMALL");
+	total += show_zones(g_malloc.large, "LARGE");
+	if (g_malloc.flags & MALLOC_HISTORY)
 		show_history();
 	printf("Total : %zu %s\n", total, total > 1 ? "bytes" : "byte");
 }
--- src/show/show_history.c
+++ src/show/show_history.c
@@ -2,7 +2,6 @@
 
 #include <stdio.h> // printf()
 
-
 static void show_log(t_history *history, size_t total, size_t start)
 {
 	static const char *op_str[] = {
@@ -11,16 +10,19 @@ static void show_log(t_history *history, size_t total, size_t start)
 		[LOG_REALLOC] = "REALLOC",
 		[LOG_CALLOC] = "CALLOC",
 	};
-
 	t_log *log;
 	size_t i;
 
 	i = 0;
 	while (i < total) {
 		log = &history->logs[(start + i) & (HISTORY_SIZE - 1)];
-		printf("#%04zu [%s] TINY %p %zu\n",
-		       i, op_str[log->op], log->ptr, log->size);
 		i++;
+		printf("#%04zu [%s] %p", i, op_str[log->op], log->ptr);
+		if (log->op == LOG_REALLOC)
+			printf(" -> %p", log->new_ptr);
+		if (log->op != LOG_FREE)
+			printf(" %zu", log->size);
+		printf("\n");
 	}
 }
 
@@ -35,7 +37,10 @@ void show_history()
 	count = history->count;
 	if (count == 0)
 		return ;
-	printf("--- HISTORY ---\n");
+	printf("--- HISTORY (%zu ops", count);
+	if (count > HISTORY_SIZE)
+		printf(", showing last %d", HISTORY_SIZE);
+	printf(") ---\n");
 	total = count < HISTORY_SIZE ? count : HISTORY_SIZE;
 	start = count < HISTORY_SIZE ? 0 : count & (HISTORY_SIZE - 1);
 	show_log(history, total, start);
