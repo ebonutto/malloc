825eefbccfb4a286b2aa4ba350344e5ec727fe84
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-07-07 17:11:34 +0200

    feat: end tests

--- tests/main.c
+++ tests/main.c
@@ -2,7 +2,7 @@
 
 #include <stddef.h> // NULL
 #include <stdio.h> // printf()
-#include <string.h> // memset()
+#include <string.h> // memcmp(), memcpy(), memset()
 
 #define GREEN "\033[0;32m"
 #define RED "\033[0;31m"
@@ -10,301 +10,324 @@
 #define BLUE "\033[0;34m"
 #define RESET "\033[0m"
 
-#define TINY_MAX 128
-#define SMALL_MAX 1024
+#define TINY_MAX 128 //TODO Update if 32 or 64 bits
+#define SMALL_MAX 1024 //TODO Update if 32 or 64 bits
 
 #define TEST_TITLE(name) printf(YELLOW  "\n--- %s ---\n" RESET, name)
 #define TEST_HEAD(name) printf(BLUE  "\n--- %s ---\n" RESET, name)
 #define TEST_PASS(name) printf(GREEN "[PASS]" RESET " %s\n", name)
 #define TEST_FAIL(name) printf(RED   "[FAIL]" RESET " %s\n", name)
 
-///* Useful to test history and hexdump */
-//static void test_show(void)
-//{
-//	void *a;
-//	void *b;
+/* Useful to test history and hexdump */
+static void test_show(void)
+{
+	void *a;
+	void *b;
 
-//	TEST_HEAD("Start Test Show");
+	TEST_HEAD("Test Show");
 
-//	/* malloc + memset */
-//	a = malloc(32);
-//	b = malloc(512);
-//	memset(a, 'A', 32);
-//	memset(b, 'B', 512);
+	/* malloc + memset */
+	a = malloc(32);
+	b = malloc(512);
+	memset(a, 'A', 32);
+	memset(b, 'B', 512);
 
-//	show_alloc_mem();
-//	TEST_PASS("show_alloc_mem()");
-//	show_alloc_mem_ex();
-//	TEST_PASS("show_alloc_mem_ex()");
+	show_alloc_mem();
+	TEST_PASS("show_alloc_mem()");
+	show_alloc_mem_ex();
+	TEST_PASS("show_alloc_mem_ex()");
 
-//	/* free */
-//	free(a);
-//	free(b);
+	/* free */
+	free(a);
+	free(b);
 
-//	show_alloc_mem();
-//	TEST_PASS("show_alloc_mem()");
-//	show_alloc_mem_ex();
-//	TEST_PASS("show_alloc_mem_ex()");
+	show_alloc_mem();
+	TEST_PASS("show_alloc_mem()");
+	show_alloc_mem_ex();
+	TEST_PASS("show_alloc_mem_ex()");
+}
 
-//	TEST_HEAD("End Test Show");
-//}
+static void test_basic(void)
+{
+	void *a;
+	void *b;
+	void *c;
 
-//static void test_zero(void)
-//{
-//	void *ptr;
+	TEST_HEAD("Test Basic");
 
-//	TEST_HEAD("Test Zero");
+	/* malloc() x 3 */
+	a = malloc(1);
+	b = malloc(42);
+	c = malloc(512);
+	if (!a || !b || !c) {
+		TEST_FAIL("malloc() x 3");
+		goto cleanup;
+	}
+	TEST_PASS("malloc() x 3");
+	show_alloc_mem_ex();
 
-//	/* malloc */
-//	ptr = malloc(0);
-//	ptr == NULL ? TEST_PASS("malloc(0) = NULL") : TEST_FAIL("malloc(0) != NULL");
+cleanup:
+	free(a);
+	free(b);
+	free(c);
+	show_alloc_mem();
+}
 
-//	/* calloc */
-//	ptr = calloc(42, 0);
-//	ptr == NULL ? TEST_PASS("calloc(42, 0) = NULL") : TEST_FAIL("calloc(42, 0) != NULL");
-//	ptr = calloc(0, 42);
-//	ptr == NULL ? TEST_PASS("calloc(0, 42) = NULL") : TEST_FAIL("calloc(0, 42) != NULL");
-//	ptr = calloc(0, 0);
-//	ptr == NULL ? TEST_PASS("calloc(0, 0) = NULL") : TEST_FAIL("calloc(0, 0) != NULL");
+static void test_zero(void)
+{
+	void *ptr;
 
-//	/* realloc */
-//	ptr = realloc(ptr, 0);
-//	ptr == NULL ? TEST_PASS("realloc(ptr, 0) = NULL") : TEST_FAIL("realloc(ptr, 0) != NULL");
-//	//ptr = realloc(NULL, 42);
-//	//ptr == NULL ? TEST_PASS("realloc(NULL, 42) = NULL") : TEST_FAIL("realloc(NULL, 42) != NULL");
-//	ptr = realloc(NULL, 0);
-//	ptr == NULL ? TEST_PASS("realloc(NULL, 0) = NULL") : TEST_FAIL("realloc(NULL, 0) != NULL");
+	TEST_HEAD("Test Zero");
 
-//	/* realloc */
-//	free(NULL);
-//	TEST_PASS("free(NULL) no crash");
+	/* malloc */
+	ptr = malloc(0);
+	ptr == NULL ? TEST_PASS("malloc(0) = NULL") : TEST_FAIL("malloc(0) != NULL");
 
-//	show_alloc_mem(); // No allocated memory expected
-//}
+	/* calloc */
+	ptr = calloc(42, 0);
+	ptr == NULL ? TEST_PASS("calloc(42, 0) = NULL") : TEST_FAIL("calloc(42, 0) != NULL");
+	ptr = calloc(0, 42);
+	ptr == NULL ? TEST_PASS("calloc(0, 42) = NULL") : TEST_FAIL("calloc(0, 42) != NULL");
+	ptr = calloc(0, 0);
+	ptr == NULL ? TEST_PASS("calloc(0, 0) = NULL") : TEST_FAIL("calloc(0, 0) != NULL");
 
-//static void test_tiny(void)
-//{
-//	void *ptrs[100];
-//	size_t i;
+	/* realloc */
+	ptr = realloc(NULL, 0);
+	ptr == NULL ? TEST_PASS("realloc(NULL, 0) = NULL") : TEST_FAIL("realloc(NULL, 0) != NULL");
 
-//	TEST_HEAD("Test Tiny");
+	/* realloc */
+	free(NULL);
+	TEST_PASS("free(NULL) no crash");
 
-//	/* Allocate memory */
-//	i = 0;
-//	while (i < 100) {
-//		ptrs[i] = malloc(TINY_MAX);
-//		if (!ptrs[i]) {
-//			TEST_FAIL("malloc(TINY_MAX) failed");
-//			while (i--)
-//				free(ptrs[i]);
-//			return ;
-//		}
-//		memset(ptrs[i], 42, TINY_MAX);
-//		i++;
-//	}
-//	TEST_PASS("malloc(TINY_MAX) x 100");
-//	show_alloc_mem_ex();
+	show_alloc_mem(); // No allocated memory expected
+}
 
-//	/* Free memory */
-//	while (i--)
-//		free(ptrs[i]);
-//	TEST_PASS("free(tiny) x 100");
-//	show_alloc_mem();
-//}
+static void test_tiny(void)
+{
+	void *ptrs[100];
+	size_t i;
 
-//static void test_small(void)
-//{
-//	void *ptrs[100];
-//	size_t i;
+	TEST_HEAD("Test Tiny");
 
-//	TEST_HEAD("Test Small");
+	/* Allocate memory */
+	i = 0;
+	while (i < 100) {
+		ptrs[i] = malloc(TINY_MAX);
+		if (!ptrs[i]) {
+			TEST_FAIL("malloc(TINY_MAX) failed");
+			while (i--)
+				free(ptrs[i]);
+			return ;
+		}
+		memset(ptrs[i], 42, TINY_MAX);
+		i++;
+	}
+	TEST_PASS("malloc(TINY_MAX) x 100");
+	show_alloc_mem_ex();
 
-//	/* Allocate memory */
-//	i = 0;
-//	while (i < 100) {
-//		ptrs[i] = malloc(SMALL_MAX);
-//		if (!ptrs[i]) {
-//			TEST_FAIL("malloc(SMALL_MAX) failed");
-//			while (i--)
-//				free(ptrs[i]);
-//			return ;
-//		}
-//		memset(ptrs[i], 42, SMALL_MAX);
-//		i++;
-//	}
-//	TEST_PASS("malloc(SMALL_MAX) x 100");
-//	show_alloc_mem_ex();
+	/* Free memory */
+	while (i--)
+		free(ptrs[i]);
+	TEST_PASS("free(tiny) x 100");
+	show_alloc_mem();
+}
 
-//	/* Free memory */
-//	while (i--)
-//		free(ptrs[i]);
-//	TEST_PASS("free(tiny) x 100");
-//	show_alloc_mem();
-//}
+static void test_small(void)
+{
+	void *ptrs[100];
+	size_t i;
 
-//static void test_large(void)
-//{
-//	void *ptrs[100];
-//	size_t i;
+	TEST_HEAD("Test Small");
 
-//	TEST_HEAD("Test Large");
+	/* Allocate memory */
+	i = 0;
+	while (i < 100) {
+		ptrs[i] = malloc(SMALL_MAX);
+		if (!ptrs[i]) {
+			TEST_FAIL("malloc(SMALL_MAX) failed");
+			while (i--)
+				free(ptrs[i]);
+			return ;
+		}
+		memset(ptrs[i], 42, SMALL_MAX);
+		i++;
+	}
+	TEST_PASS("malloc(SMALL_MAX) x 100");
+	show_alloc_mem_ex();
 
-//	/* Allocate memory */
-//	i = 0;
-//	while (i < 100) {
-//		ptrs[i] = malloc(1025);
-//		if (!ptrs[i]) {
-//			TEST_FAIL("malloc(1025) failed");
-//			while (i--)
-//				free(ptrs[i]);
-//			return ;
-//		}
-//		memset(ptrs[i], 42, 1025);
-//		i++;
-//	}
-//	TEST_PASS("malloc(1025) x 100");
-//	show_alloc_mem_ex();
+	/* Free memory */
+	while (i--)
+		free(ptrs[i]);
+	TEST_PASS("free(tiny) x 100");
+	show_alloc_mem();
+}
 
-//	/* Free memory */
-//	while (i--)
-//		free(ptrs[i]);
-//	TEST_PASS("free(tiny) x 100");
-//	show_alloc_mem();
-//}
+static void test_large(void)
+{
+	void *ptrs[100];
+	size_t i;
 
-//static void test_fragmentation(void)
-//{
-//	void *a;
-//	void *b;
-//	void *c;
-//	void *d;
+	TEST_HEAD("Test Large");
 
-//	TEST_HEAD("Test Fragmentation");
+	/* Allocate memory */
+	i = 0;
+	while (i < 100) {
+		ptrs[i] = malloc(1025);
+		if (!ptrs[i]) {
+			TEST_FAIL("malloc(1025) failed");
+			while (i--)
+				free(ptrs[i]);
+			return ;
+		}
+		memset(ptrs[i], 42, 1025);
+		i++;
+	}
+	TEST_PASS("malloc(1025) x 100");
+	show_alloc_mem_ex();
 
-//	/* Initial setup */
-//	a = malloc(32);
-//	b = malloc(32);
-//	c = malloc(32);
-//	d = malloc(32);
-//	if (!a || !b || !c || !d) {
-//		TEST_FAIL("malloc() failed");
-//		goto cleanup;
-//	}
-//	show_alloc_mem_ex();
+	/* Free memory */
+	while (i--)
+		free(ptrs[i]);
+	TEST_PASS("free(tiny) x 100");
+	show_alloc_mem();
+}
 
-//	/* Free middle */
-//	free(b);
-//	free(c);
-//	show_alloc_mem_ex();
+static void test_realloc(void)
+{
+	char *ptr;
+	char *new_ptr;
 
-//	/* Fat chunk in the middle */
-//	b = malloc(72);
-//	c = malloc(32);
-//	if (!b || !c) {
-//		TEST_FAIL("malloc() failed");
-//		goto cleanup;
-//	}
-//	show_alloc_mem_ex();
+	TEST_HEAD("Test Realloc");
 
-//	/* Free middle */
-//	free(b);
-//	free(c);
-//	show_alloc_mem_ex();
+	ptr = malloc(16);
+	if (!ptr)
+		return (void)TEST_FAIL("Initial malloc()");
+	memcpy(ptr, "Hello, World!!!", 16);
+	TEST_PASS("Initial malloc()");
 
-//	/* Chunk with 0 data in the middle */
-//	b = malloc(56);
-//	c = malloc(32);
-//	if (!b || !c) {
-//		TEST_FAIL("malloc() failed");
-//		goto cleanup;
-//	}
-//	show_alloc_mem_ex();
+	/* Grow */
+	new_ptr = realloc(ptr, 128);
+	if (!new_ptr) {
+		free(ptr);
+		return (void)TEST_FAIL("Realloc grow");
+	}
+	TEST_PASS("Realloc grow");
+	show_alloc_mem_ex();
+	ptr = new_ptr;
 
-//	/* Free middle */
-//	free(b);
-//	free(c);
-//	show_alloc_mem_ex();
+	/* Data preserved */
+	memcmp(ptr, "Hello, World!!!", 16) == 0 ? TEST_PASS("Realloc data preserved") : TEST_FAIL("Realloc data preserved");
 
-//	/* Go back to initial setup */
-//	b = malloc(32);
-//	c = malloc(32);
-//	if (!b || !c) {
-//		TEST_FAIL("malloc() failed");
-//		goto cleanup;
-//	}
-//	show_alloc_mem_ex();
+	/* Shrink */
+	new_ptr = realloc(ptr, 8);
+	if (!new_ptr) {
+		free(ptr);
+		return (void)TEST_FAIL("Realloc shrink");
+	}
+	TEST_PASS("Realloc shrink");
+	show_alloc_mem_ex();
+	ptr = new_ptr;
+	free(ptr);
 
-//cleanup:
-//	free(a);
-//	free(b);
-//	free(c);
-//	free(d);
-//	show_alloc_mem();
-//}
+	/* realloc(NULL) == malloc() */
+	new_ptr = realloc(NULL, 64);
+	new_ptr != NULL ? TEST_PASS("realloc(NULL) == malloc()") : TEST_FAIL("realloc(NULL) == malloc()");
+	free(new_ptr);
 
-//static void test_realloc(void)
-//{
-//	char *ptr;
-//	char *new_ptr;
+	show_alloc_mem();
+}
 
-//	TEST_HEAD("REALLOC");
+static void test_fragmentation(void)
+{
+	void *a;
+	void *b;
+	void *c;
+	void *d;
 
-//	ptr = malloc(16);
-//	if (!ptr)
-//	{
-//		TEST_FAIL("realloc initial malloc");
-//		return ;
-//	}
-//	memcpy(ptr, "Hello, World!!!", 16);
+	TEST_HEAD("Test Fragmentation");
 
-//	/* Agrandir */
-//	new_ptr = realloc(ptr, 128);
-//	if (!new_ptr)
-//	{
-//		TEST_FAIL("realloc grow");
-//		free(ptr);
-//		return ;
-//	}
-//	memcmp(new_ptr, "Hello, World!!!", 16) == 0
-//		? TEST_PASS("realloc grow data preserved")
-//		: TEST_FAIL("realloc grow data preserved");
-//	ptr = new_ptr;
+	/* Initial setup */
+	a = malloc(32);
+	b = malloc(32);
+	c = malloc(32);
+	d = malloc(32);
+	if (!a || !b || !c || !d) {
+		TEST_FAIL("malloc() failed");
+		goto cleanup;
+	}
+	TEST_PASS("Initial setup");
+	show_alloc_mem_ex();
 
-//	/* Réduire */
-//	new_ptr = realloc(ptr, 8);
-//	new_ptr != NULL
-//		? TEST_PASS("realloc shrink")
-//		: TEST_FAIL("realloc shrink");
-//	ptr = new_ptr;
+	/* Free middle */
+	free(b);
+	free(c);
+	TEST_PASS("Free middle");
+	show_alloc_mem_ex();
 
-//	/* realloc(NULL) == malloc */
-//	new_ptr = realloc(NULL, 64);
-//	new_ptr != NULL
-//		? TEST_PASS("realloc(NULL) == malloc")
-//		: TEST_FAIL("realloc(NULL) == malloc");
-//	free(new_ptr);
+	/* Fat chunk in the middle */
+	b = malloc(72);
+	c = malloc(32);
+	if (!b || !c) {
+		TEST_FAIL("malloc() failed");
+		goto cleanup;
+	}
+	TEST_PASS("Fat chunk in the middle");
+	show_alloc_mem_ex();
 
-//	/* realloc(ptr, 0) == free */
-//	new_ptr = realloc(ptr, 0);
-//	new_ptr == NULL
-//		? TEST_PASS("realloc(ptr, 0) == free")
-//		: TEST_FAIL("realloc(ptr, 0) == free");
+	/* Free middle */
+	free(b);
+	free(c);
+	TEST_PASS("Free middle");
+	show_alloc_mem_ex();
 
-//	show_alloc_mem();
-//}
+	/* Chunk with 0 data in the middle */
+	b = malloc(56);
+	c = malloc(32);
+	if (!b || !c) {
+		TEST_FAIL("malloc() failed");
+		goto cleanup;
+	}
+	TEST_PASS("Chunk with 0 data in the middle");
+	show_alloc_mem_ex();
+
+	/* Free middle */
+	free(b);
+	free(c);
+	TEST_PASS("Free middle");
+	show_alloc_mem_ex();
+
+	/* Go back to initial setup */
+	b = malloc(32);
+	c = malloc(32);
+	if (!b || !c) {
+		TEST_FAIL("malloc() failed");
+		goto cleanup;
+	}
+	TEST_PASS("Go back to initial setup");
+	show_alloc_mem_ex();
+
+	/* Free all */
+cleanup:
+	free(a);
+	free(b);
+	free(c);
+	free(d);
+	TEST_PASS("Free all");
+	show_alloc_mem();
+}
 
 int main(void)
 {
 	TEST_TITLE("Start Test malloc");
 
-	//test_show();
-	//test_zero();
-	//test_basic();
-	//test_tiny();
-	//test_small();
-	//test_large();
-	//test_stress();
-	//test_fragmentation();
+	test_show();
+	test_zero();
+	test_basic();
+	test_tiny();
+	test_small();
+	test_large();
+	test_realloc();
+	test_fragmentation();
 
 	TEST_TITLE("End Test malloc");
 	return (0);
