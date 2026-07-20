7743750b543fbf7dc6fa01c1df84ebf4b153a536
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-07-20 11:48:08 +0200

    feat: add MALLOC_DATA

--- src/env/env.c
+++ src/env/env.c
@@ -5,8 +5,10 @@
 __attribute__((constructor))
 static void init_env(void)
 {
-	if (getenv("MALLOC_HISTORY"))
-		g_malloc.flags |= MALLOC_HISTORY;
+	if (getenv("MALLOC_DATA"))
+		g_malloc.flags |= MALLOC_DATA;
 	if (getenv("MALLOC_HEXDUMP"))
 		g_malloc.flags |= MALLOC_HEXDUMP;
+	if (getenv("MALLOC_HISTORY"))
+		g_malloc.flags |= MALLOC_HISTORY;
 }
