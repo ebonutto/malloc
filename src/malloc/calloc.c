62c11dde1c211b2766da4154af289b89d447f641
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-07-20 16:31:41 +0200

    fix: overflow in history

--- src/malloc/calloc.c
+++ src/malloc/calloc.c
@@ -1,31 +1,34 @@
 #include "malloc_int.h"
 
-#include <stddef.h> // NULL
+#include <pthread.h> // pthread_mutex_lock(), pthread_mutex_unlock()
+#include <stddef.h> // size_t, NULL
 #include <stdint.h> // SIZE_MAX
 #include <strings.h> // bzero()
-
-static void *calloc_impl(size_t count, size_t size)
-{
-	void *ptr;
-
-	if (count != 0 && size > SIZE_MAX / count)
-		return (NULL);
-	size = count * size;
-	ptr = malloc_impl(size);
-	if (!ptr)
-		return (NULL);
-	bzero(ptr, size);
-	return (ptr);
-}
 
 void *calloc(size_t count, size_t size)
 {
 	void *ptr;
+	size_t total;
 
 	pthread_mutex_lock(&g_malloc.lock);
-	ptr = calloc_impl(count, size);
+
+	if (count != 0 && size > SIZE_MAX / count) {
+		if (g_malloc.flags & MALLOC_HISTORY)
+			history_push(LOG_CALLOC, NULL, NULL, 0);
+
+		pthread_mutex_unlock(&g_malloc.lock);
+		return (NULL);
+	}
+
+	total = count * size;
+
+	ptr = malloc_impl(total);
+	if (ptr)
+		bzero(ptr, total);
+
 	if (g_malloc.flags & MALLOC_HISTORY)
-		history_push(LOG_CALLOC, ptr, NULL, count * size); //! count * size can overflow
+			history_push(LOG_CALLOC, ptr, NULL, total);
+
 	pthread_mutex_unlock(&g_malloc.lock);
 	return (ptr);
 }
